a1c91006d6466756e32a1b79388a685c3e936bb4
wesleiads <adspositivo2022@gmail.com>
2022-09-25 23:01:21 -0300

    changes class

--- app/src/main/java/com/example/minefield/GameClass.java
+++ app/src/main/java/com/example/minefield/GameClass.java
@@ -5,6 +5,9 @@ import static com.example.minefield.R.*;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.TextView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,16 +15,16 @@ import androidx.appcompat.app.AppCompatActivity;
 public class GameClass extends AppCompatActivity {
     private Button ButtonPlayAgain; //ButtonRestart
     private static final int[] idArray = {
-            R.id.button_1,R.id.button_2,
-            R.id.button_3,R.id.button_4,
-            R.id.button_5,R.id.button_6,
-            R.id.button_7,R.id.button_8,
-            R.id.button_9,R.id.button_10,
-            R.id.button_11,R.id.button_12,
-            R.id.button_13,R.id.button_14,R.id.button_15,R.id.button_16
+            R.id.button_1, R.id.button_2,
+            R.id.button_3, R.id.button_4,
+            R.id.button_5, R.id.button_6,
+            R.id.button_7, R.id.button_8,
+            R.id.button_9, R.id.button_10,
+            R.id.button_11, R.id.button_12,
+            R.id.button_13, R.id.button_14, R.id.button_15, R.id.button_16
 
     };
-    private Button[] button = new Button[idArray.length];
+    private Button[] buttons = new Button[idArray.length];
 
     int i;
 
@@ -33,14 +36,24 @@ public class GameClass extends AppCompatActivity {
         ButtonPlayAgain = findViewById(R.id.button_play_again);
 
 
-        for(int i = 0; i < idArray.length; i++){
+        for (int i = 0; i < idArray.length; i++) {
 
             //int y = i +1;
-            button[i] = (Button)findViewById(idArray[i]);
+            buttons[i] = (Button) findViewById(idArray[i]);
 
-            button[i].setText("Works");
+            buttons[i].setText("123");
 
+
+            ButtonPlayAgain.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    TextView viewById = (TextView) findViewById(id.button_1);
+
+
+                    viewById.setText("");
+                }
+            });
         }
-    }
 
+    }
 }
